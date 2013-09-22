e54adf32a7d6b3262c7fb695f779e0065d73a8d2
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-09-21 23:02:32 -0700

    gtfo grumpig

--- mods/fairyou/moves.js
+++ mods/fairyou/moves.js
@@ -1,1 +1,35 @@
-//moves.js? more like moves.hue
+exports.BattleMovedex = {	
+	"pixiepunch": {
+		num: 7765,
+		accuracy: 100,
+		basePower: 85,
+		category: "Physical",
+		desc: "nigga please.",
+		shortDesc: "ggg chance to ggg the target.",
+		id: "pixiepunch",
+		isViable: true,
+		name: "Pixie Punch",
+		pp: 15,
+		priority: 0,
+		isContact: true,
+		secondary: false
+		target: "normal",
+		type: "Fairy"
+	},
+	"fairybeam": {
+		num: 8253,
+		accuracy: 100,
+		basePower: 85,
+		category: "Special",
+		desc: "lies",
+		shortDesc: "more lies.",
+		id: "fairybeam",
+		isViable: true,
+		name: "Fairy Beam",
+		pp: 15,
+		priority: 0,
+		secondary: false
+		target: "normal",
+		type: "Fairy"
+	}	
+};
